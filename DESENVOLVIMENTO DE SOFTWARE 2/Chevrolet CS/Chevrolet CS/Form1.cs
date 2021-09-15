e26ccf9790cd9f943adc4af008a3e655e1969a43
Breno Moura <89282568+Breno-Moura@users.noreply.github.com>
2021-09-14 22:10:51 -0300

    Add files via upload

---
+++ DESENVOLVIMENTO DE SOFTWARE 2/Chevrolet CS/Chevrolet CS/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chevrolet_CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tableBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.bandodecadosDataSet1);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'bandodecadosDataSet1.Table' table. You can move, or remove it, as needed.
+            this.tableTableAdapter.Fill(this.bandodecadosDataSet1.Table);
+
+        }
+
+        private void modeloToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.tableTableAdapter.modelo(this.bandodecadosDataSet1.Table, modeloToolStripTextBox.Text);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ DESENVOLVIMENTO DE SOFTWARE 2/conectar com banco de dados/conectar com banco de dados/formmenu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace conectar_com_banco_de_dados
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+
+        private void formmenu_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'bando_de_CadosDataSet.tabalunos' table. You can move, or remove it, as needed.
+            this.tabalunosTableAdapter.Fill(this.bando_de_CadosDataSet.tabalunos);
+
+        }
+
+        private void opção1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabalunosBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tabalunosBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.bando_de_CadosDataSet);
+
+        }
+    }
+}
