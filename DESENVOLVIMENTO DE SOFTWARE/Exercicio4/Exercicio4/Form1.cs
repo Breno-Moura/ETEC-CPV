3e63155763ef0753ca1347740b9025e91328b2ce
Breno Moura <89282568+Breno-Moura@users.noreply.github.com>
2021-09-14 22:03:21 -0300

    Add files via upload

---
+++ DESENVOLVIMENTO DE SOFTWARE/Exercicio4/Exercicio4/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Double f,c;
+            c = double.Parse((textBox1.Text));
+            f = (9 * c + 160) / 5;
+            label3.Text =f.ToString();
+        }
+    }
+}
---
+++ DESENVOLVIMENTO DE SOFTWARE/Exercicio5/Exercicio5/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Double a, b, c, d, m;
+
+            a = double.Parse(textBox2.Text);
+            b = double.Parse(textBox3.Text);
+            c = double.Parse(textBox4.Text);
+            d = double.Parse(textBox5.Text);
+            m = (a + b + c + d) / 4;
+            label9.Text = m.ToString();
+        }
+    }
+}
